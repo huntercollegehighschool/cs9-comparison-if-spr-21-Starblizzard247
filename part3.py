d1c7445b62d009620c3f4a4238184066c1eaa64c
Stefani Myung <103528392+Starblizzard247@users.noreply.github.com>
2022-05-10 03:06:19 +0000

    wrote the code

--- part3.py
+++ part3.py
@@ -20,3 +20,15 @@ not divisible by 3
 '''
 
 #write your code below
+number = int(input("Enter a number: "))
+if number < 0:
+  print("negative")
+elif number == 0:
+  print("zero")
+else:
+  print("positive")
+
+if number % 3:
+  print("not divisible by 3")
+else:
+  print("divisible by 3")
